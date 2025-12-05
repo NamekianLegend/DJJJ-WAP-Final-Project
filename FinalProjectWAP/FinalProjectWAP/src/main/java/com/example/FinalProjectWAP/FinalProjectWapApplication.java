88710f7e9d16d8e428757691f0ab41d512c0a733
Josh Leslie <joshleslie1993@gmail.com>
2025-12-05 14:12:49 -0400

    added spring initializer folder

---
+++ FinalProjectWAP/FinalProjectWAP/src/main/java/com/example/FinalProjectWAP/FinalProjectWapApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinalProjectWAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectWapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectWapApplication.class, args);
+	}
+
+}
