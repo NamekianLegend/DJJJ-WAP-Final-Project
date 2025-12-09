3aea8b7fdbc0fd03e36fb1da8521a8d2394795fd
BOOG77 <dawsonb14@hotmail.com>
2025-12-09 19:40:46 -0400

    created mockmvc test

---
+++ FinalProjectWapRevised/src/test/java/com/example/FinalProjectWAPRevised/mockmvctest/InvalidLoginTest.java
@@ -0,0 +1,29 @@
+package com.example.FinalProjectWAPRevised.mockmvctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class InvalidLoginTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testInvalidLogin() throws Exception {
+        mockMvc.perform(post("/login").param("username", "invalidUsername").param("password", "invalidPassword"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("loginError", "Invalid Login"))
+                .andExpect(view().name("login"))
+                .andDo(print());
+    }
+}
