4a06fde30f2624da9d6cacf466f064382cf15cf5
Josh Leslie <joshleslie1993@gmail.com>
2025-12-10 10:55:26 -0400

    deleted old version
    blaaaaaaaaaah

--- FinalProjectWapRevised/src/main/java/com/example/FinalProjectWAPRevised/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.FinalProjectWAPRevised.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.FinalProjectWAPRevised.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer findByUsername(String username);
-}
--- FinalProjectWapRevised/src/main/java/com/example/FinalProjectWAPRevised/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.FinalProjectWAPRevised.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.FinalProjectWAPRevised.model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-    Item findByTitle(String title);
-}
